1289893e8c66b35af5211aedec31ce1c25fbdb7a
A.Amani <didi1364@agmail.com>
2018-08-29 19:23:27 +0430

    i forgot some files

---
+++ build/CMakeFiles/surena.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/surena.dir/surena.cc.o"
+  "libsurena.pdb"
+  "libsurena.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/surena.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
